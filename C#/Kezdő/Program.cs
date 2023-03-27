0804f8e428967d8a22398798923b06f0bea0bb5f
Marcii0 <58266292+Marcii0@users.noreply.github.com>
2023-03-27 16:30:49 +0200

    Add files via upload

---
+++ C#/Kezdő/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            do
+            {
+                Console.Write("Adjon meg egy számot: ");
+
+                a = int.Parse(Console.ReadLine());
+                Console.Write("Adjon meg egy második számot: ");
+
+                b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Mit szeretne tenni?:\n1) Összeadás\n2) Kivonás\n3) Szorzás\n4) Osztás");
+
+                int valasztas = int.Parse(Console.ReadLine());
+                if (valasztas == 1) Console.WriteLine(a + b);
+                else if (valasztas == 2) Console.WriteLine(a - b);
+                else if (valasztas == 3) Console.WriteLine(a * b);
+                else if (valasztas == 4) Console.WriteLine(a / b);
+                else Console.WriteLine("Helytelen menüpontot adott meg.");
+                Console.WriteLine("Szeretne-e még számolni?");
+            } while (Console.ReadLine() == "igen");
+        }
+    }
+}
---
+++ C#/Közép Haladó/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg egy szöveget: ");
+            string bekert = Console.ReadLine();
+            Console.Write("Adjon meg egy karaktert: ");
+            char karakter = Console.ReadKey().KeyChar;
+            int tartalomszam = 0;
+            for (int k = 0; k < bekert.Length; k++)
+                if (bekert[k] == karakter)
+                    tartalomszam++;
+            Console.WriteLine($"{tartalomszam} alkalommal jelent meg a '{karakter}' karakter.");
+
+            tartalomszam = 0;
+
+            for (int k = 0; k < bekert.Length; k++)
+                if (bekert[k] == 'a')
+                    tartalomszam++;
+
+            Console.WriteLine($"{tartalomszam} alkalommal jelent meg az 'a' karakter.");
+        }
+    }
+}
